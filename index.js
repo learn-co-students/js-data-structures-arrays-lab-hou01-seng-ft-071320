ebc02fd922e8de90bf1cd063a6dc5aca3821e6d4
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-08-25 13:20:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function appendDriver(name) {
+
+    let d = drivers.slice()
+    d.push(name)
+    return d 
+
+}
+
+function prependDriver(name) {
+
+    let d = drivers.slice()
+    d.unshift(name)
+    return d 
+
+}
+
+function removeLastDriver() {
+
+    let d = drivers.slice()
+    d.pop()
+    return d
+
+}
+
+function removeFirstDriver() {
+
+    let d = drivers.slice()
+    d.shift()
+    return d
+
+}
